407434207be0baef0e91db783f80f5a8b049c664
SteveDMurphy <steven.d.murphy@gmail.com>
2022-02-18 16:35:21 -0500

    incorporate remaining requested changes

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -1,4 +1,4 @@
-//create the database, role, and warehouse
+-- create the database, role, and warehouse
 
 use role accountadmin;
 create database if not exists raw;
@@ -6,28 +6,28 @@ create warehouse if not exists fides_log;
 
 use role securityadmin;
 
-create role if not exists fides_log;
+create role if not exists event_writer;
 
 
-//add a user for fideslog
+-- add a user for fideslog
 
-create user fides_log password=******************* default_role=fides_log default_warehouse=fides_log default_namespace='raw'; -- replace the asterisks with an appropriate password
+create user event_writer password=******************* default_role=event_writer default_warehouse=fides_log default_namespace='raw'; -- replace the asterisks with an appropriate password
 
-// grants for role and user (I know I am forgetting some here...)
+-- grants for role and user
 
-grant usage on warehouse fides_log to role fides_log;
+grant usage on warehouse fides_log to role event_writer;
 
-grant usage, create schema, monitor on database raw to role fides_log;
+grant usage, create schema, monitor on database raw to role event_writer;
 
-grant usage on future schemas in database raw to role fides_log;
+grant usage on future schemas in database raw to role event_writer;
 
-grant role fides_log to role sysadmin;
+grant role event_writer to role sysadmin;
 
-grant role fides_log to user fides_log;
+grant role event_writer to user event_writer;
 
-// create the schema and table
+-- create the schema and table
 
-use role fides_log;
+use role event_writer;
 use warehouse fides_log;
 use database raw;
 
@@ -58,6 +58,22 @@ create table if not exists anonymous_usage_events (
 
 create table if not exists api_keys (
   api_key varchar,
+  client_id varchar,
   created_at timestamp_tz,
   expired_at timestamp_tz
 )
+
+
+show tables in schema raw.fides;
+
+
+-- downgrade, destroy, etc.
+
+/*
+use role accountadmin;
+-- drop warehouse fides_log;
+drop database raw;
+use role securityadmin;
+drop role fides_log;
+drop user fides_log;
+*/

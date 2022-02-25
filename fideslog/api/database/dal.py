3e8ba4ba25253008d869555b099d863a1545ec4e
Phil Salant <PSalant@gmail.com>
2022-02-24 19:35:13 -0500

    WIP: Begin coding database interactions

---
+++ fideslog/api/database/dal.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+from models.analytics_event import AnalyticsEvent
+from models import APIKey, AnalyticsEvent as AnalyticsEventORM
+
+
+# TODO: Finish this
+def create_event(db: Session, event: AnalyticsEvent) -> AnalyticsEventORM:
+    """Create a new analytics event."""
+
+    db.add(AnalyticsEventORM())
+
+
+def api_key_exists(db: Session, token: str) -> bool:
+    """
+    Return whether the provided token exists in the database.
+    """
+
+    result = db.execute(select(APIKey).where(APIKey.api_key == token))
+    return len(result) > 0

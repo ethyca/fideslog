98534cf1d0642b7ee2bca01f8ead829728b6d649
Phil Salant <PSalant@gmail.com>
2022-03-08 17:58:27 -0500

    Remove last remaining API key reference

--- fideslog/sdk/python/client.py
+++ fideslog/sdk/python/client.py
@@ -18,7 +18,6 @@ class AnalyticsClient:
 
     def __init__(
         self,
-        api_key: str,
         client_id: str,
         os: str,
         product_name: str,
@@ -28,7 +27,6 @@ class AnalyticsClient:
         """
         Define a new client from which to send analytics events to the fideslog server.
 
-        :param api_key: The authorization token, administered by Ethyca, that enables query access to the fideslog server.
         :param client_id: The fully anonymized, globally unique identifier for this client.
         :param os: The operating system on which this client is running, as returned by [platform.system()](https://docs.python.org/3/library/platform.html#platform.system).
         :param product_name: The name of the fides tool in which this client is integrated.
@@ -36,12 +34,11 @@ class AnalyticsClient:
         :param extra_data: Any additional information that should be included in all analytics events sent by this client. Any key/value pairs included here will be merged with key/value pairs included directly on specific `AnalyticsEvent`s, with the `AnalyticsEvent`'s `extra_data` taking priority.
         """
 
-        for arg in [api_key, client_id, os, product_name, production_version]:
+        for arg in [client_id, os, product_name, production_version]:
             assert arg is not None and arg != "", (
                 f"{arg=}".split("=")[0] + " must be provided"
             )
 
-        self.api_key = api_key
         self.client_id = client_id
         self.os = os
         self.product_name = product_name

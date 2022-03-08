6a583df2070036ea49bd3480164d8fd1c8c0c83f
Phil Salant <PSalant@gmail.com>
2022-03-08 17:50:35 -0500

    Log event creation errors to server logs

--- fideslog/api/endpoints/events.py
+++ fideslog/api/endpoints/events.py
@@ -1,14 +1,15 @@
+from logging import getLogger
 from typing import Dict
 
 from fastapi import APIRouter, Depends, status
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
-from fideslog.api.models.analytics_event import AnalyticsEvent
 from fideslog.api.database.data_access import create_event
 from fideslog.api.database.database import get_db
 from fideslog.api.models.analytics_event import AnalyticsEvent
 
+log = getLogger(__name__)
 router = APIRouter(tags=["Events"], prefix="/events")
 
 
@@ -18,15 +19,12 @@ router = APIRouter(tags=["Events"], prefix="/events")
     response_model=AnalyticsEvent,
     status_code=status.HTTP_201_CREATED,
 )
-async def create(
-    event: Dict,
-    database: Session = Depends(get_db),
-) -> AnalyticsEvent:
+async def create(event: Dict, database: Session = Depends(get_db)) -> AnalyticsEvent:
     """Create a new analytics event."""
     try:
         event_obj = AnalyticsEvent.parse_obj(event)
         create_event(database=database, event=event_obj)
-    except ValidationError:
-        print("Write a simple error to the db for traceability")
+    except ValidationError as e:
+        log.error(e.__str__())
 
     return event_obj

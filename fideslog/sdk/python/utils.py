1c52a2a1fdc7df9993b60c2a8ab40e35bfcd8be9
SteveDMurphy <steven.d.murphy@gmail.com>
2022-03-10 09:39:40 -0500

    add opt_out_copy to sdk utils

--- fideslog/sdk/python/utils.py
+++ fideslog/sdk/python/utils.py
@@ -6,6 +6,17 @@ from bcrypt import gensalt
 FIDESCTL_API = b"fidesctl-api"
 FIDESCTL_CLI = b"fidesctl-cli"
 FIDESOPS = b"fidesops"
+OPT_OUT_COPY = """
+Fides needs your permission to send Ethyca a limited set of anonymous usage statistics.
+Ethyca will only use this anonymous usage data to improve the product experience, and will never collect sensitive or personal data.
+
+***
+Don't believe us? Check out the open-source code here:
+    https://github.com/ethyca/fideslog
+***
+
+To opt-out of all telemetry, press "n". To continue with telemetry, press any other key.
+"""
 
 
 def generate_client_id(application: bytes) -> str:

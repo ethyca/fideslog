01e3fe71ef5a581a13104b981e01e5e111b37982
Phil Salant <PSalant@gmail.com>
2022-04-08 19:00:33 -0400

    Use the required header in tests

--- tests/api/test_routes.py
+++ tests/api/test_routes.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 def test_health() -> None:
     """Test that the /health endpoint responds"""
 
-    response = client.get("/health")
+    response = client.get("/health", headers={"X-Fideslog-Version": "1.0.0"})
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"status": "healthy"}

301665b4151d88b89d4f40a738bc54900db01ea9
Phil Salant <PSalant@gmail.com>
2022-10-24 16:04:38 -0600

    Remove unused `pathlib` import, coalesce requirements more clearly

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
-import pathlib
-
 from setuptools import setup
 
 from versioneer import get_cmdclass, get_version
 
-here = pathlib.Path(__file__).parent.resolve()
-long_description = open("./fideslog/sdk/python/README.md").read()
-
-# Requirements
-install_requires = (
-    open("fideslog/sdk/python/requirements.txt").read().strip().split("\n")
-)
-dev_requires = open("dev-requirements.txt").read().strip().split("\n")
+api_requires = open("./fideslog/api/requirements.txt").read().strip().split("\n")
+sdk_requires = open("./fideslog/sdk/python/requirements.txt").read().strip().split("\n")
+dev_requires = open("./dev-requirements.txt").read().strip().split("\n")
 
 setup(
     name="fideslog",
     version=get_version(),
     cmdclass=get_cmdclass(),
     description="SDK for interacting with fideslog",
-    long_description=long_description,
+    long_description=open("./README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ethyca/fideslog",
     python_requires=">=3.8, <4",
@@ -27,7 +20,7 @@ setup(
     author="Ethyca, Inc.",
     author_email="fidesteam@ethyca.com",
     license="Apache License 2.0",
-    install_requires=install_requires,
+    install_requires=api_requires + sdk_requires,
     dev_requires=dev_requires,
     classifiers=[
         "License :: OSI Approved :: Apache Software License",

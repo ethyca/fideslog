d357aaba32d1e4de824ff5a2236368d512f1cb48
Andrew Jackson <andrew.c.j1995@gmail.com>
2022-10-28 15:40:40 -0400

    Remove registration from event router

--- fideslog/api/routes/events.py
+++ fideslog/api/routes/events.py
@@ -4,11 +4,10 @@ from fastapi import APIRouter, Depends, Request, status
 from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import Session
 
-from ..database.data_access import create_event, create_user_registration_event
+from ..database.data_access import create_event
 from ..database.database import get_db
 from ..errors import InternalServerError, TooManyRequestsError
 from ..schemas.analytics_event import AnalyticsEvent
-from ..schemas.user_registration_event import UserRegistrationEvent
 
 log = getLogger(__name__)
 event_router = APIRouter(tags=["Events"], prefix="/events")
@@ -39,30 +38,3 @@ async def create(
         raise InternalServerError(err) from err
 
     return event
-
-
-@event_router.post(
-    "/user-registration",
-    response_description="The created event",
-    response_model=UserRegistrationEvent,
-    responses={
-        status.HTTP_429_TOO_MANY_REQUESTS: TooManyRequestsError.doc(),
-        status.HTTP_500_INTERNAL_SERVER_ERROR: InternalServerError.doc(),
-    },
-    status_code=status.HTTP_201_CREATED,
-)
-async def create_user_registration_event_route(
-    _: Request,
-    user_registration_event: UserRegistrationEvent,
-    database: Session = Depends(get_db),
-) -> UserRegistrationEvent:
-    """
-    Create a new user registration event.
-    """
-
-    try:
-        create_user_registration_event(database=database, event=user_registration_event)
-    except DBAPIError as err:
-        raise InternalServerError(err) from err
-
-    return user_registration_event
